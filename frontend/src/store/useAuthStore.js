3cad1bd58e9aeb398e9b14eb1b90d5126518739e
Darshik <rajparadarshi@gmail.com>
2025-01-06 21:01:35 +0530

    i think now fixed.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -92,7 +92,7 @@ export const useAuthStore = create((set, get) => ({
             // Prevent duplicate socket connections
             if (get().socket?.connected) return;
 
-            const socket = io("http://localhost:5001", {
+            const socket = io(BASE_URL, {
                 query: { userId: authUser._id },
             });
 
